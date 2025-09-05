03840ad694a4204865a8ac49142d68756b9aff1e
artur-bolt <artur.boltrukiewicz@anatomiq.dev>
2025-09-05 15:03:32 +0200

    Update

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   name         = format("%s-%s-%s", var.prefix, var.environment, var.app)
-  cluster_name = var.eks_cluster != "" ? var.eks_cluster : format("%s-%s-eks", var.prefix, var.environment)
+  cluster_name = var.eks_cluster
   tags = merge(
     var.tags,
     {
